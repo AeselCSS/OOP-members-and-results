7b57a9e8abf423866a8586898a9f0354613487ca
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-27 20:25:27 +0200

    added imports, awaits and displayMembers to "init" function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import initTabs from "./assets/modules/tabs.js";
-import {buildMemberList, buildResultList} from "./assets/modules/utils/utils.js";
+import {buildMemberList, buildResultList, members, results} from "./assets/modules/utils/utils.js";
+import displayMembers from "./assets/modules/display-members.js";
 
-window.addEventListener('load', ():void => {
+window.addEventListener('load', async (): Promise<void> => {
     initTabs();
-    buildMemberList();
-    buildResultList();
+    await buildMemberList();
+    await buildResultList();
+    console.log(results)
+    displayMembers(members);
 });
 
