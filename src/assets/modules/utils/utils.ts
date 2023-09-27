06e8ee893a2d39c0d6c3fd08a855637385882a07
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-27 19:03:28 +0200

    added utility functions for fetching and building lists of members and results

---
+++ src/assets/modules/utils/utils.ts
@@ -0,0 +1,33 @@
+import * as member from "../construct-member.js";
+import * as result from "../construct-result.js";
+let members: Member[] = [];
+let results: Result[] = [];
+console.log(members)
+console.log(results)
+async function fetchMembers () : Promise<MemberJson[]> {
+    const data = await fetch (`./assets/data/members.json`);
+    return await data.json();
+}
+
+async function fetchResults () : Promise<ResultJson[]> {
+    const data = await fetch (`./assets/data/results.json`);
+    return await data.json();
+}
+
+
+async function buildMemberList (): Promise<void> {
+    const membersArr = await fetchMembers();
+    for (const memberObj of membersArr) {
+        members.push(member.construct(memberObj));
+    }
+}
+
+async function buildResultList (): Promise<void> {
+    const resultsArr = await fetchResults();
+    for (const resultObj of resultsArr) {
+        results.push(result.construct(resultObj));
+    }
+}
+
+
+export { members, results, buildMemberList, buildResultList };
