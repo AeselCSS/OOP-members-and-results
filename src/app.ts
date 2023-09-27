d30d80320b99b4db998e4f4eae74f84b5f5df5c2
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-27 23:02:41 +0200

    added displayResults

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import initTabs from "./assets/modules/tabs.js";
 import {buildMemberList, buildResultList, members, results} from "./assets/modules/utils/utils.js";
 import displayMembers from "./assets/modules/display-members.js";
+import displayResults from "./assets/modules/display-results.js";
 
 window.addEventListener('load', async (): Promise<void> => {
     initTabs();
     await buildMemberList();
     await buildResultList();
-    console.log(results)
     displayMembers(members);
+    displayResults(results);
 });
 
