3fcc21703ccc691665eb008a99354758490c62f1
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-09-27 13:19:22 +0200

    added constructor for members

---
+++ src/modules/construct-member.ts
@@ -0,0 +1,41 @@
+function construct(memberObject: MemberJson): Member {
+    const member: Member = {
+        firstName: memberObject.firstName,
+        lastName: memberObject.lastName,
+        active: memberObject.isActiveMember,
+        birthDate: new Date(memberObject.dateOfBirth),
+        image: memberObject.image,
+        renderDateOfBirth: (date: Date) => {
+            return new Intl.DateTimeFormat('da-DK', {
+                year: "numeric",
+                month: "long",
+                day: "numeric"
+            }).format(date);
+        },
+        get age() {
+            const now = new Date();
+            const age = now.getFullYear() - this.birthDate.getFullYear();
+            if (
+                now.getMonth() < this.birthDate.getMonth() ||
+                (now.getMonth() === this.birthDate.getMonth() &&
+                    now.getDate() < this.birthDate.getDate())
+            ) {
+                return age - 1;
+            } else {
+                return age;
+            }
+        },
+        isJunior: () => {
+            return member.age < 18;
+        },
+        isSenior: () => {
+            return member.age >= 18;
+        },
+        get fullName() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+    };
+    return member;
+}
+
+export { construct };
